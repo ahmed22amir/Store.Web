f6c08f630df1a1b7128c3b484db5b0822fba3823
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-11 16:43:55 +0300

    RepositoryLayer&SeedData

--- Store.Data/Entity/Product.cs
+++ Store.Data/Entity/Product.cs
@@ -10,11 +10,12 @@ namespace Store.Data.Entity
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         public decimal Price { get; set; }
-        public int BrandId { get; set; }
-        public ProductBrand ProductBrand { get; set; }
+        public ProductType Type { get; set; }
         public int TypeId { get; set; }
-        public ProductType ProductType { get; set; }
+        public ProductBrand Brand { get; set; }
+        public int BrandId { get; set; }
+ 
     }
 }
