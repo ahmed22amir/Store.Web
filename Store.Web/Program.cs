1946b2a7529488f30e612105f3ca9843d8c877fe
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-11 15:05:00 +0300

    ProjectSetupAndDataLayer

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Store.Data.Context;
+
 namespace Store.Web
 {
     public class Program
@@ -13,6 +16,12 @@ namespace Store.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+            });
 
             var app = builder.Build();
 
