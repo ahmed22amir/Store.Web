4902960e4990fd2f970424301949a7b512b82706
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-11 17:07:52 +0300

    RepositoryLayer-GenericRepository

---
+++ Store.Repository/InterFaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Store.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.InterFaces
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        Task<TEntity> GetByIdAsync(TKey? id);
+        Task<IReadOnlyList<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
