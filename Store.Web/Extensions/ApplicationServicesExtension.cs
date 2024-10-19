7ae5fbb7f3be3104aa85a184dfd6c3b3ec9ea767
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-19 19:56:09 +0300

    ProgramRefactor

---
+++ Store.Web/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,18 @@
+﻿using Store.Repository.InterFaces;
+using Store.Repository.UnitOfWork;
+using Store.Service.Services.Products.Dtos;
+using Store.Service.Services.Products;
+
+namespace Store.Web.Extensions
+{
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection ApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddAutoMapper(typeof(ProductProfile));
+            return services;
+        }
+    }
+}
--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -5,6 +5,7 @@ using Store.Repository.InterFaces;
 using Store.Repository.UnitOfWork;
 using Store.Service.Services.Products;
 using Store.Service.Services.Products.Dtos;
+using Store.Web.Extensions;
 using Store.Web.Helper;
 using Store.Web.Middleware;
 
@@ -22,15 +23,17 @@ namespace Store.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
-            builder.Services.AddScoped<IProductService,ProductService>();
-            builder.Services.AddAutoMapper(typeof(ProductProfile));
+            //builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+            //builder.Services.AddScoped<IProductService,ProductService>();
+            //builder.Services.AddAutoMapper(typeof(ProductProfile));
+            builder.Services.ApplicationServices();
             builder.Services.AddDbContext<StoreDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 
             });
 
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
