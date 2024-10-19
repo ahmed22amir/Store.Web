a9845977efd5e49ec69c255d656b2160280a1e46
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-19 19:24:33 +0300

    MiddlewareClassesAndStrucure

---
+++ Store.Service/HandelResponse/CustomerExeption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Service.HandelResponse
+{
+    public class CustomerExeption : Response
+    {
+        public CustomerExeption(int statusCode, string? message = null, string? details= null) : base(statusCode, message)
+        {
+            Details=details;
+        }
+        public string? Details { get; set; }
+    }
+}
---
+++ Store.Service/HandelResponse/Response.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Store.Service.HandelResponse
+{
+    public class Response
+    {
+        public Response(int statusCode, string? message = null)
+        {
+            StatusCode = statusCode;
+            Message = GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+            => statusCode switch
+            {
+                100=> "Continue",
+                101 => "Switching Protocols",
+                200 => "OK",
+                201 => "Created",
+                204 => "No Content",
+                301 => "Moved Permanently",
+                302 => "Found",
+                304 => "Not Modifie",
+                400 => "Bad Request",
+                401 => "Unauthorized",
+                403 => "Forbidden",
+                404 => "Not Found",
+                500 => "Internal Server Error",
+                502 => "Bad Gateway",
+                503 => "Service Unavailable",
+            };
+    }
+}
