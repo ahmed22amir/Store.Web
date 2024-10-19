5649bc279184deb96343a2624dde844c3e8e48a8
Amir <Amir@DESKTOP-VLHUF4M>
2024-10-19 17:51:30 +0300

    OrderSpecification

--- Store.Repository/Specification/ISpecification.cs
+++ Store.Repository/Specification/ISpecification.cs
@@ -10,7 +10,9 @@ namespace Store.Repository.Specification
     public interface ISpecification<T>
     {
         Expression<Func<T,bool>> Criteria { get; }
-
         List<Expression<Func<T, object>>> Includes { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+
     }
 }
--- Store.Service/Services/Products/ProductService.cs
+++ Store.Service/Services/Products/ProductService.cs
@@ -57,7 +57,8 @@ namespace Store.Service.Services.Products
             {
                 throw new Exception("Id is null");
             }
-            var product = await _unitOfWork.Repository<Product, int>().GetByIdAsync(id.Value);
+            var specs = new ProductWithSpecification(id);
+            var product = await _unitOfWork.Repository<Product, int>().GetWithSpecificationByIdAsync(specs);
             if (product is null)
             {
                 throw new Exception("product not found");
